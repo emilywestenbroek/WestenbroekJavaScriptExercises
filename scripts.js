a920026506500480f0b1abb7392c2ff2eab0ce14
emilywestenbroek <em.westenbroek@gmail.com>
2018-01-08 19:50:56 -0500

    Moving html and js to github

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Exercise 1 -- for loop that starts at zero, adds one to total until
+// sum is equal to number entered by user
+
+var sum = "Please enter a number";
+
+var number = prompt(sum);
+
+alert(number);
+
+for (var i=0; var i>=number; i++) {
+  // console.log(i); << I think this will just list the numbers b/w 0 and what is entered by the user
+  result += i;
+}
+
+alert(result);
+
+//-------------------
+
+// Exercise 2
+
+var play = prompt("Do you want to play?");
+
+do {
+  promt()
+} while ();
