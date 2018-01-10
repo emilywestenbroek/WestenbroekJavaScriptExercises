c4787316d338f809066ae6a3dd0cb970267cfb6c
emilywestenbroek <em.westenbroek@gmail.com>
2018-01-09 19:12:32 -0500

    Complete Exercise 1 and 2

--- scripts.js
+++ scripts.js
@@ -1,25 +1,47 @@
 // Exercise 1 -- for loop that starts at zero, adds one to total until
 // sum is equal to number entered by user
 
-var sum = "Please enter a number";
+// // var sum = "Please enter a number";
+//
+// var number = prompt("Please enter a number");
+// var result = 0;
+//
+// for (var i=0; i<=number; i++) {
+//  result += i;
+// }
+//
+// alert(result);
 
-var number = prompt(sum);
+// //-------------------
+//
+// // Exercise 2
+//
 
-alert(number);
 
-for (var i=0; var i>=number; i++) {
-  // console.log(i); << I think this will just list the numbers b/w 0 and what is entered by the user
-  result += i;
-}
-
-alert(result);
-
-//-------------------
-
-// Exercise 2
-
+var accumulator = "";
 var play = prompt("Do you want to play?");
 
-do {
-  promt()
-} while ();
+if (play === "yes") {
+  do {
+      word = prompt("Enter a word.");
+      accumulator = accumulator + " " + word;
+      play = prompt("Do you want to play again?");
+  } while (play === "yes");
+  console.log (accumulator);
+}
+
+// while (!play.equalsIgnoreCase("yes") && !play.equalsIgnoreCase("no")) {
+//     if (play.equalsIgnoreCase("yes")) {
+//         var firstWord = prompt(word) {
+//
+//         }
+//         break;
+//     } else (play.equalsIgnoreCase("no")) {
+//         alert(firstWord + " " + secondWord + " " + thirdWord);
+//         break;
+//     }
+// }
+//
+// //---------------------------------
+//
+// // Exercise 3
